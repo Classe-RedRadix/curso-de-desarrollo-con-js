58b86dc32888e977804c90d618c91188029ceb79
Enrique <enrique@redradix.com>
2022-10-01 18:57:46 +0200

    Removed solutions to promote sending solutions by PR

--- modulo-1-fundamentos/soluciones/ejercicio-variables-II.js
+++
@@ -1,9 +0,0 @@
-function randomNumber(modifier) {
-  let base; // <- hay que mover el let fuera del bloque
-  if (Math.random() > 0.5) {
-    base = 1;
-  } else {
-    base = -1;
-  }
-  return base * modifier * Math.random();
-}
