279e43fc8668f8399e7965b00925cb3db7c48cd2
Ines <ines@redradix.com>
2022-10-06 18:11:48 +0200

    Finished exercise-II, module 2

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-II.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-II.js
@@ -1,4 +1,36 @@
-// Crea aquí tu función cut
+const cut = (array, firstIndex, lastIndex) => {
+  const list = [];
+  const finalIndex = lastIndex ?? array.length - 1;
 
-cut(["a", "b", "c", "d"], 1, 2); // ["b", "c"]
-cut(["a", "b", "c", "d"], 1); // ["b", "c", "d"]
+  for (let i = firstIndex; i <= finalIndex; i++) {
+    list.push(array[i]);
+  }
+
+  return list;
+};
+
+// ["b", "c"]
+console.log(cut(["a", "b", "c", "d"], 1, 2));
+
+// ["b", "c", "d"]
+console.log(cut(["a", "b", "c", "d"], 1));
+
+const cut2 = (array, firstIndex, lastIndex) => {
+  const finalIndex = lastIndex ?? array.length - 1;
+
+  const list = array.reduce((acc, value, index) => {
+    if (index >= firstIndex && index <= finalIndex) {
+      return [...acc, value];
+    } else {
+      return acc;
+    }
+  }, []);
+
+  return list;
+};
+
+// ["b", "c"]
+console.log(cut2(["a", "b", "c", "d"], 1, 2));
+
+// ["b", "c", "d"]
+console.log(cut2(["a", "b", "c", "d"], 1));
