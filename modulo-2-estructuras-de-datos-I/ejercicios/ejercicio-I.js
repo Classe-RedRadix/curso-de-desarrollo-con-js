90a3890494ffa655c57f58cc18d75ef716d0b334
Ines <ines@redradix.com>
2022-10-06 18:11:39 +0200

    Finished exercise-I, module 2

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-I.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-I.js
@@ -1,1 +1,15 @@
-// Hacerlo en clase
+const printItems = (array) => {
+  if (!array.length) return;
+
+  if (array.length === 1) {
+    console.log(array[0]);
+    return;
+  }
+
+  console.log(array[0], array[array.length - 1]);
+};
+
+printItems([]);
+printItems([1]);
+printItems([1, 2, 3]);
+printItems([1, 2]);
