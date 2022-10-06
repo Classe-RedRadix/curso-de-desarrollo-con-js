6fbf04b4f5be446c7d55351c767fba6037aecb20
Ines <ines@redradix.com>
2022-10-06 18:12:05 +0200

    Finished exercise-III, module 2

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-III.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-III.js
@@ -1,7 +1,33 @@
 // Crea aquí tu función flatten
+const flatten = (matrix) => {
+  const flatArray = [];
 
-flatten([
-  ["A1", "A2", "A3"],
-  ["B1", "B2", "B3"],
-  ["C1", "C2", "C3"],
-]); // => ["A1", "A2", "A3", "B1", "B2", ...]
+  matrix.forEach((array) => {
+    array.forEach((item) => flatArray.push(item));
+  });
+
+  return flatArray;
+};
+
+//["A1", "A2", "A3", "B1", "B2", ...]
+console.log(
+  "flatten",
+  flatten([
+    ["A1", "A2", "A3"],
+    ["B1", "B2", "B3"],
+    ["C1", "C2", "C3"],
+  ])
+);
+
+const flatten2 = (matrix) =>
+  matrix.reduce((acc, current) => [...acc, ...current], []);
+
+//["A1", "A2", "A3", "B1", "B2", ...]
+console.log(
+  "flatten2",
+  flatten2([
+    ["A1", "A2", "A3"],
+    ["B1", "B2", "B3"],
+    ["C1", "C2", "C3"],
+  ])
+);
