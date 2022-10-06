9b691f39d86261712f6c0274260e1af70b35bfde
Enrique <enrique@redradix.com>
2022-10-06 20:45:40 +0200

    Added exercises module 2

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-filter.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-filter.js
@@ -1,4 +1,8 @@
-[
+// Crea una función que devuelva solo los valores impares de esta lista
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// ❗ Opcional: Obten solo los objetos con el valor "important" a true
+const list = [
   { name: "lorem", important: false },
   { name: "ipsum", important: true },
 ];
--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
@@ -1,2 +1,3 @@
 // Calcular la suma de todos los números de un array.
+
 // Concatenar todas las strings de un array.
