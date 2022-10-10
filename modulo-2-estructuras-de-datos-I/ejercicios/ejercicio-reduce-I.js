f640530f2e399578e144435dbfb0420211887d31
juanantonioledesma <juanledesma@redradix.com>
2022-10-10 21:04:13 +0200

    Update ejercicio-reduce-I.js

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
@@ -1,3 +1,5 @@
 // Calcular la suma de todos los números de un array.
+console.log([1, 3, 5, 7, 11, 13].reduce((acc, cur) => acc + cur, 0))
 
 // Concatenar todas las strings de un array.
+console.log(['a', 'be', 'ce', 'da', 'rio'].reduce((acc, cur) => acc + cur, ''))
