f661b67b7a2620fec1d0c2180bab1292b429d3c4
Ines <ines@redradix.com>
2022-10-10 17:40:00 +0200

    :sparkles: resolve reduce exercice

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-reduce-I.js
@@ -1,2 +1,12 @@
 // Calcular la suma de todos los números de un array.
+const numbersToSum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const sum = numbersToSum.reduce((sum, number) => (sum = sum + number), 0);
+console.log(sum); //45
+
 // Concatenar todas las strings de un array.
+const letters = ["a", "b", "c", "d"];
+const concatenatedString = letters.reduce(
+  (string, letter) => (string = string + letter),
+  ""
+);
+console.log(concatenatedString);
