eaf020f06daea29f9f8e7b80c014e998a446eb8b
juanantonioledesma <juanledesma@redradix.com>
2022-10-10 20:58:18 +0200

    Update ejercicio-map-III.js

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-map-III.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-map-III.js
@@ -1,1 +1,14 @@
+const array = [[1, 2], [34, 20, 5], [11], [2, 4]]
+console.log(
+  array.map(subArray => {
+    let subArraySum = 0
+
+    subArray.forEach(number => {
+      subArraySum += number
+    })
+
+    return subArraySum
+  }),
+)
+
 // [[1, 2], [34, 20, 5], [11], [2, 4]] -> [3, 59, 11, 6]
