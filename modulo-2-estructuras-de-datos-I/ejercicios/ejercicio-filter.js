85d0523cdcb137755a281d593404e7ece0ed670c
Ines <ines@redradix.com>
2022-10-10 17:39:29 +0200

    :sparkles: resolve filter exercice

--- modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-filter.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/ejercicio-filter.js
@@ -1,4 +1,10 @@
-[
-  { name: "lorem", important: false },
-  { name: "ipsum", important: true },
+//Dada una lista de números, conserva los números impares.
+const evenNumbers = [1, 2, 3, 4, 5, 6].filter((number) => number % 2 === 0);
+console.log(evenNumbers);
+
+//Dada una lista de objetos, conserva los objetos que tengan una propiedad important: true.
+const list = [
+  ({ name: "lorem", important: false }, { name: "ipsum", important: true }),
 ];
+const importantList = list.filter((item) => item.important);
+console.log(importantList);
