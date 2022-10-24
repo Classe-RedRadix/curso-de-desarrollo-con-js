02275861a62582e9bef6050fd9337639051000cb
Ines <ines@redradix.com>
2022-10-24 18:15:34 +0200

    :sparkles: destructuring-III done

--- modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-destructuring-III.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-destructuring-III.js
@@ -2,7 +2,11 @@ const obj = { uno: 1, lista: [2, 3], cuatro: 4, x: { cinco: 5 } };
 
 // Desestructura el siguiente objeto en las variables uno, dos, tres, cuatro y cinco.
 
-/**
- * Pistas:
- * 1. Usa destructuración anidada.
- */
+const {
+  uno,
+  lista: [dos, tres],
+  cuatro,
+  x: { cinco },
+} = obj;
+
+console.log(uno, dos, tres, cuatro, cinco);
