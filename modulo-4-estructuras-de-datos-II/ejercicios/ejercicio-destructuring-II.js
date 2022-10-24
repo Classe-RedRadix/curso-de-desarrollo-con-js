6695b7fbec3ff6db50612554babe9311bc58a4f6
Ines <ines@redradix.com>
2022-10-24 18:10:43 +0200

    :sparkles: destructuring-II done

--- modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-destructuring-II.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-destructuring-II.js
@@ -4,5 +4,6 @@ let b = 2;
 // Intercambia el valor de a y b, sin crear una tercera variable,
 // usando destructurazión.
 // No es necesario que destructures un objeto.
+[a, b] = [2, 1];
 
 console.log(a, b); // 2 1
