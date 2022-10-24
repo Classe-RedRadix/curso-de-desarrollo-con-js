5a5c1abea8e452b233f4c0d13ebe9e8520b973ca
Ines <ines@redradix.com>
2022-10-24 18:35:04 +0200

    :sparkles: resolve getter.setters excercie

--- modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-getter-setters.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-getter-setters.js
@@ -1,10 +1,18 @@
 const array = [1, 2, 3];
 
 // Añade una propiedad average a un array que devuelva la media de los valores del array.
-
-console.log(array.average); // Deberiamos obtener 2
-
 /**
  * Pistas:
  * 1. Puedes usar Object.defineProperty sobre un array.
  */
+
+const getAverage = (arr) => {
+  const sum = arr.reduce((acc, num) => (acc = acc + num), 0);
+  return sum / arr.length;
+};
+
+Object.defineProperty(array, "average", {
+  value: getAverage(array),
+});
+
+console.log(array.average); // Deberiamos obtener 2
