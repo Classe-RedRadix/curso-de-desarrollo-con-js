89e72cb9ba2de5d2c22256ba84a5da90561f63a7
Ines <ines@redradix.com>
2022-10-24 19:37:46 +0200

    :sparkles: resolve ejercicio-objetos-III

--- modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-objetos-III.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/ejercicio-objetos-III.js
@@ -2,14 +2,6 @@ const a = { a: 1 };
 const b = { b: 2 };
 const c = { c: 3 };
 
-Object.assign(a, b, c);
+const obj = Object.assign({}, a, b, c);
 
-console.log(a); // { a: 1, b: 2, c: 3 }
-
-// ¿Como obtenemos el mismo resultado, sin modificar a?
-
-/**
- * Pistas:
- * 1. Si modificamos de izquierda a derecha,
- *    es posible que necesitemos un elemento extra a la izquierda.
- */
+console.log(obj); // { a: 1, b: 2, c: 3 }
