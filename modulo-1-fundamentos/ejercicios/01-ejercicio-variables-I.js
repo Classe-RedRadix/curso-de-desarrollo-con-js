6603aebffe82eba5dc9e5c995973e2ed7458ce81
Belén Moreno Hurtado <bmoreno@gmv.com>
2023-01-16 19:01:39 +0100

    Add work in class for day 1

--- modulo-1-fundamentos/ejercicios/01-ejercicio-variables-I.js
+++ modulo-1-fundamentos/ejercicios/01-ejercicio-variables-I.js
@@ -6,13 +6,14 @@
  */
 
 function randomNumber(modifier) {
+  let base;
   if (Math.random() > 0.5) {
-    let base = 1;
+     base = 1;
   } else {
-    let base = -1;
+     base = -1;
   }
 
   return base * modifier * Math.random();
 }
 
-randomNumber(); // Error!
+console.log(randomNumber(1)); // Error!
--- modulo-1-fundamentos/ejercicios/02-ejercicio-variables-II.js
+++ modulo-1-fundamentos/ejercicios/02-ejercicio-variables-II.js
@@ -7,9 +7,9 @@
 
 function createIndexLoggers() {
   let list = []; // <- Este array contendrá funciones, que ejecutan un console.log (linea 5 a la 7)
-
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     list.push(function () {
+      console.log("dentro de función: ");
       console.log(i);
     });
   }
@@ -20,5 +20,10 @@ const loggers = createIndexLoggers();
 const firstLogger = loggers[0]; // Primera función dentro de la variable list (dentro de la función)
 const secondLogger = loggers[1]; // Segunda función
 
-firstLogger(); // ?
-secondLogger(); // ?
+console.log("fuera: ");
+console.log(firstLogger()); // ?
+
+console.log("fuera: ");
+console.log(secondLogger()); // ?
+
+console.log(Math.max);
