8b4daac985b5e6566a40e7b30b0ceaab114268ca
Belén Moreno Hurtado <bmoreno@gmv.com>
2023-01-26 17:04:28 +0100

    Add the most completed exercise until now

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
@@ -1,42 +1,41 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
-const dir = "../../ejercicio-final";
+function asyncMap(list, transfomer, callback) {
+  const result = [];
 
-function calculateDirSize(dir, callback) {
-    let dirSize = 0;
-    let filesCount = 0;
-    let filesRead = 0;
+  list.forEach((element, index) => {
+    transfomer(element, (value) => {
+      result[index] = value;
 
-    fs.readdir(dir, (error, files) => {
-        if (error) throw error;
-        console.log("Files: ", files);
-        const filesPath = files.map((file) => path.resolve(dir, file));
-
-        filesCount = filesPath.length;
-
-        filesPath.forEach((file, index) => {
-            console.log("Start asynchronous process ", file, index);
-
-            fs.stat(file, (error, stats) => {
-                if (error) throw error;
-
-                filesRead++;
-                dirSize += stats.size;
-
-                console.log(file, stats.size);
-                
-                if (filesRead === filesCount) {
-                    callback(dirSize); 
-                }
-            });
-        });
-
-        
+      if (list.length === result.length) {
+        callback(result);
+      }
     });
+  });
 }
 
-calculateDirSize(dir, function(size) {
-    console.log(size);
-});
+function calculateDirSize(dir, callback) {
+  fs.readdir(dir, (error, fileNames) => {
+    if (error) throw error;
+    const files = fileNames.map((file) => path.resolve(dir, file));
+
+    function mapFileToSize(file, done) {
+      fs.stat(file, (error, stats) => {
+        if (error) throw error;
+
+        done(stats.size);
+      });
+    }
+
+    function onListMapped(fileSizes) {
+      const sum = fileSizes.reduce((total, n) => total + n, 0);
+      callback(sum);
+    }
+
+    asyncMap(files, mapFileToSize, onListMapped);
+  });
+}
+
+calculateDirSize("../../ejercicio-final", (size) => console.log(size)); // 6763
 
