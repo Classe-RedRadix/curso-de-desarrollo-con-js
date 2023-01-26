25b1de6c8a59afc21199e7c0a0899d81f81446d7
Belén Moreno Hurtado <bmoreno@gmv.com>
2023-01-26 16:45:34 +0100

    Change code for dynamic loop for asynchronous calls

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
@@ -5,22 +5,38 @@ const dir = "../../ejercicio-final";
 
 function calculateDirSize(dir, callback) {
     let dirSize = 0;
+    let filesCount = 0;
+    let filesRead = 0;
 
     fs.readdir(dir, (error, files) => {
         if (error) throw error;
         console.log("Files: ", files);
         const filesPath = files.map((file) => path.resolve(dir, file));
 
-        fs.stat(filesPath[0], (error, fileStat) => {
-            if (error) throw error;
-            console.log(filesPath[0], fileStat.size);
-            dirSize += fileStat.size;
-            callback(dirSize); 
-        })
+        filesCount = filesPath.length;
+
+        filesPath.forEach((file, index) => {
+            console.log("Start asynchronous process ", file, index);
+
+            fs.stat(file, (error, stats) => {
+                if (error) throw error;
+
+                filesRead++;
+                dirSize += stats.size;
+
+                console.log(file, stats.size);
+                
+                if (filesRead === filesCount) {
+                    callback(dirSize); 
+                }
+            });
+        });
+
+        
     });
 }
 
-calculateDirSize(dir, function(something) {
-    console.log(something);
+calculateDirSize(dir, function(size) {
+    console.log(size);
 });
 
