6c0d6da5183894e42afec4d56436579d69c25aa5
Belén Moreno Hurtado <bmoreno@gmv.com>
2023-01-26 16:39:57 +0100

    Add asynchronous fixed solution

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-dir-size.js
@@ -3,18 +3,24 @@ const path = require('path');
 
 const dir = "../../ejercicio-final";
 
-function calculateDirSize(dir) {
+function calculateDirSize(dir, callback) {
+    let dirSize = 0;
+
     fs.readdir(dir, (error, files) => {
         if (error) throw error;
         console.log("Files: ", files);
-        const filesPath = result.map((file) => path.resolve(dir, file));
+        const filesPath = files.map((file) => path.resolve(dir, file));
 
         fs.stat(filesPath[0], (error, fileStat) => {
             if (error) throw error;
             console.log(filesPath[0], fileStat.size);
+            dirSize += fileStat.size;
+            callback(dirSize); 
         })
     });
 }
 
-calculateDirSize(dir);
+calculateDirSize(dir, function(something) {
+    console.log(something);
+});
 
