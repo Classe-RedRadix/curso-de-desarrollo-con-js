711d85557433d03be385a88189431fc4802cc3de
Belén Moreno Hurtado <bmoreno@gmv.com>
2023-02-06 17:58:07 +0100

    Add final exercise

--- ejercicio-final/ejercicio.js
+++ ejercicio-final/ejercicio.js
@@ -1,10 +1,29 @@
-// Implementa como se especifica en README.md el ejercicios
 
 const futureValue = (value, millisecondsWait) =>
   new Promise((resolve) => setTimeout(() => resolve(value), millisecondsWait));
+  
 
 function asynk(generator) {
-  // ?
+  return function (...args) {
+    const gen = generator(...args);
+  
+    function iterator(yielded) {
+      return new Promise(async (resolve) => {
+
+        // Si hemos llegado al final, devolvemos
+        if (yielded.done) return(yielded.value);
+
+        // Wrappeamos en caso de que yielded.value no sea una promesa
+        const promise = Promise.resolve(yielded.value);
+
+        // Resolvemos la promesa yieldeada con la siguiente llamada a `next`
+        resolve(iterator(gen.next(await promise)));
+      });
+    }
+
+    return iterator(gen.next());
+  
+  };
 }
 
 const main = asynk(function* () {
@@ -17,14 +36,14 @@ const main = asynk(function* () {
    * 👇🏻 (Opcional) Descomentar cuando funcione el anterior,
    * deberia funcionar sin hacer cambios en nuestra función "asynk".
    */
-  // const letters = ["C", "L", "A", "S", "S", "E"];
+  const letters = ["C", "L", "A", "S", "S", "E"];
 
-  // for (let index = 0; index < letters.length; index++) {
-  //   const wait = index * 1000 || 1000;
-  //   const letter = yield futureValue(letters[index], wait);
+  for (let index = 0; index < letters.length; index++) {
+    const wait = index * 1000 || 1000;
+    const letter = yield futureValue(letters[index], wait);
 
-  //   console.log("Letter #%d is %s", index, letter);
-  // }
+    console.log("Letter #%d is %s", index, letter);
+  }
 });
 
 main();
