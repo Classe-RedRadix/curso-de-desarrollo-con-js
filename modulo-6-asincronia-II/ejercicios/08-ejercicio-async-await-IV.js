688c56f68751133c1c7e8629c3788abcc53a2191
Enrique <enrique@redradix.com>
2023-05-16 16:02:07 +0200

    Add missing exercises

---
+++ modulo-6-asincronia-II/ejercicios/08-ejercicio-async-await-IV.js
@@ -0,0 +1,11 @@
+/**
+ * Implementa la función promiseAllSafe que recibe una lista de promesas y devuelve una
+ * lista con los resultados.
+ * - Si la promesa se resuelve, añade el resultado a la lista.
+ * - Si la promesa falla, añade null a la lista e imprime el error por consola.
+ * - Las promesas se deben lanzar secuencialmente
+ */
+
+async function promiseAllSafe() {
+  // ?
+}
