6e2301cf5212a4332f93e24cbbe56678d8b2307f
Mario Alvarez <mario.alvarez@gmail.com>
2023-06-11 00:16:18 +0200

    Support for nested asyncs

--- ejercicio-final/ejercicio.js
+++ ejercicio-final/ejercicio.js
@@ -1,19 +1,32 @@
 // Implementa como se especifica en README.md el ejercicios
 
 const futureValue = (value, millisecondsWait) =>
-  new Promise((resolve) => setTimeout(() => resolve(value), millisecondsWait));
+  new Promise((resolve) => 
+      setTimeout(() => resolve(value), millisecondsWait)
+    );
 
   function asynk(generator) {
     const gen = generator();
-  
-    function recursive_asynk(v) {
-      const next = gen.next(v);
-      if (next.done === true) {
-        return;
+    
+    // we have to return a function
+    return function() {
+      // this is the inner function which does the recursive calls. 
+      // Its argument is the next() return
+      function recursive_asynk(next_result) {
+        return new Promise((resolve, reject) => {
+          if (next_result.done === true) {
+            return resolve();
+          }
+
+          next_result.value.then((val) => {
+            resolve(recursive_asynk(gen.next(val)));
+          }).catch(() => reject(new Error("Asynk call Error")));
+        });
       }
-      next.value.then((value) => {recursive_asynk(value)}).catch(() => { throw new Error("Asynk call error") });
+
+      // Here starts everything, with the first next call
+      return recursive_asynk(gen.next());
     }
-    return recursive_asynk;
   }
 
 const main = asynk(function* () {
@@ -33,3 +46,21 @@ const main = asynk(function* () {
 });
 
 main();
+
+// Test nested asynk 
+const main2 = asynk(function* () {
+  yield asynk(function* () {
+    const three = yield futureValue(3, 1000);
+    console.log(three);
+    const four = yield futureValue(4, 1000);
+    console.log(four);
+  })();
+  yield asynk(function* () {
+    const five = yield futureValue(5, 1000);
+    console.log(five);
+    const six = yield futureValue(6, 1000);
+    console.log(six);
+  })();
+});
+
+main2();
