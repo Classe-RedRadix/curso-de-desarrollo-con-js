26d6370ce23f29b78745c7e5e75f4c2e251b617a
lauradefi <laudefilippis@libero.it>
2023-06-12 12:38:52 +0200

    Ejercicio Final - Laura De Filippis

--- ejercicio-final/ejercicio.js
+++ ejercicio-final/ejercicio.js
@@ -4,8 +4,30 @@ const futureValue = (value, millisecondsWait) =>
   new Promise((resolve) => setTimeout(() => resolve(value), millisecondsWait));
 
 function asynk(generator) {
-  // ?
+  //Llamamos al generador y con el return devolvemos una funcionn para llamar al main();
+  return () => {
+    const iter = generator();
+    //Definimos la variable de output
+    let futureResult;
+    // Describimos la funcion de iteración para poderla hacer de forma recursiva
+    (function iteration(value) {
+      // iteramos sobre el generador
+      futureResult = iter.next(value);
+      //Si estoy en el ultimo yield
+      if (futureResult.done) {
+        futureResult.value;
+      } else {
+        // Aqui hacemos el .then y el .catch dependiendo si no podemos o podemos resolver la promisa
+        futureResult.value
+          .then(iteration)
+          .catch(err => {
+            err
+          });
+      }
+    })();
+  }
 }
+
 
 const main = asynk(function* () {
   const one = yield futureValue(1, 1000);
@@ -13,18 +35,17 @@ const main = asynk(function* () {
   const two = yield futureValue(2, 1000);
   console.log(two); // 2 -> a los dos segundos
 
-  /**
-   * 👇🏻 (Opcional) Descomentar cuando funcione el anterior,
-   * deberia funcionar sin hacer cambios en nuestra función "asynk".
-   */
-  // const letters = ["C", "L", "A", "S", "S", "E"];
 
-  // for (let index = 0; index < letters.length; index++) {
-  //   const wait = index * 1000 || 1000;
-  //   const letter = yield futureValue(letters[index], wait);
+  const letters = ["C", "L", "A", "S", "S", "E"];
+  
+  for (let index = 0; index < letters.length; index++) {
+     const wait = index * 1000 || 1000;
+     const letter = yield futureValue(letters[index], wait);
 
-  //   console.log("Letter #%d is %s", index, letter);
-  // }
+  console.log("Letter #%d is %s", index, letter);
+  }
+  // Esto es para probar si el catch funciona
+  yield Promise.reject(console.log("Error: promise no solved"));
 });
 
 main();
