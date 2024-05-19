58c7f5d4d8365b1955ff42131a1826cbc0274f50
martitasea <marta.pulido.asin@gmail.com>
2024-05-19 22:21:15 +0200

    chunk

---
+++ prework/chunk.js
@@ -0,0 +1,20 @@
+// Escribe una función que dados un array y una longitud `size`
+// divida la lista en múltiples subarrays de longitud `size`
+
+function chunk(array, size) {
+  let result = [];
+  for (let i = 0; i <= array.length-1; i = i + size) {
+    let chunkToInsert = [array.slice(i, i + size)];
+    result = result.concat(chunkToInsert);
+  }
+  return result
+}
+
+console.log(chunk([1, 2, 3, 4], 2))
+
+// Ejemplos
+//chunk([1, 2, 3, 4], 2) === [[ 1, 2], [3, 4]]
+//chunk([1, 2, 3, 4, 5], 2) === [[ 1, 2], [3, 4], [5]]
+//chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) === [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+//chunk([1, 2, 3, 4, 5], 4) === [[ 1, 2, 3, 4], [5]]
+//chunk([1, 2, 3, 4, 5], 10) === [[ 1, 2, 3, 4, 5]]
