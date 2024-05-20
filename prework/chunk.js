a0f23cdee7319cfc96be940248c9a5686943624f
martitasea <marta.pulido.asin@gmail.com>
2024-05-20 10:20:19 +0200

    chunk2

--- prework/chunk.js
+++ prework/chunk.js
@@ -4,7 +4,7 @@
 function chunk(array, size) {
   let result = [];
   for (let i = 0; i <= array.length-1; i = i + size) {
-    let chunkToInsert = [array.slice(i, i + size)];
+    let chunkToInsert = array.slice(i, i + size);
     result = result.concat(chunkToInsert);
   }
   return result
@@ -13,7 +13,7 @@ function chunk(array, size) {
 console.log(chunk([1, 2, 3, 4], 2))
 
 // Ejemplos
-//chunk([1, 2, 3, 4], 2) === [[ 1, 2], [3, 4]]
+chunk([1, 2, 3, 4], 2) === [[ 1, 2], [3, 4]]
 //chunk([1, 2, 3, 4, 5], 2) === [[ 1, 2], [3, 4], [5]]
 //chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) === [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 //chunk([1, 2, 3, 4, 5], 4) === [[ 1, 2, 3, 4], [5]]
